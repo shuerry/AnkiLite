4cd5ef0902225af2fefed4282c6bb9e6fa5f6e27
shuerry <sherrychn2003@gmail.com>
2023-05-21 17:16:48 -0400

    Question class and difficulty enum added. Fixes #2

---
+++ src/main/java/cs3500/pa02/Difficulty.java
@@ -0,0 +1,6 @@
+package cs3500.pa02;
+
+public enum Difficulty {
+  EASY,
+  HARD
+}
---
+++ src/main/java/cs3500/pa02/Question.java
@@ -0,0 +1,25 @@
+package cs3500.pa02;
+
+public class Question {
+  String question;
+  String answer;
+  Difficulty difficulty;
+
+  Question(String question, String answer, Difficulty difficulty) {
+    this.question = question;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+
+  public String getQuestion() {
+    return this.question;
+  }
+
+  public String getAnswer() {
+    return this.answer;
+  }
+
+  public void setDifficulty(Difficulty difficulty) {
+    this.difficulty = difficulty;
+  }
+}
